6812fae9311b66e06e4e00086a5d405803064e4e
Muharrem <ozcankocabiyik07@gmail.com>
2023-12-26 03:09:04 +0300

    26.12 (1.0)

---
+++ project_14/page-1/js/detailpage.js
@@ -0,0 +1,15 @@
+// ./js/detailpage.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Sayfa yüklendiğinde çalışacak kodlar buraya gelebilir.
+
+  // addButton elemanını seçin
+  var addButton = document.getElementById('addButtonnn');
+
+  // addButton'a tıklama olayını ekle
+  addButton.addEventListener('click', function () {
+      // Buraya butona tıklandığında yapılmasını istediğiniz işlemleri ekleyin.
+      alert('Butona tıklandı!');
+      // Örnek: Bir fonksiyonu çağırmak: myFunction();
+  });
+});
